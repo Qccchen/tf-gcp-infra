fb1b4af2fba18fc86f734814164ca11227d25fb6
ChenChenChen99 <115513462+ChenChenChen99@users.noreply.github.com>
2024-02-16 16:31:20 -0800

    Gcp tf (#6)
    * Update tf files
    
    * Update tf files
    
    ---------
    
    Co-authored-by: QianChen <chen.qian6@northeastern.edu>

--- main.tf
+++ main.tf
@@ -1,27 +1,28 @@
 resource "google_compute_network" "my_vpc" {
-  name                            = "my-vpc"
+  name                            = var.vpc_name
+  routing_mode                    = var.routing_mode
   auto_create_subnetworks         = false
   delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp_subnet" {
-  name          = "webapp"
+  name          = var.webapp_subnet_name
   network       = google_compute_network.my_vpc.id
   ip_cidr_range = var.webapp_subnet_cidr
   region        = var.region
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
-  name          = "db"
+  name          = var.db_subnet_name
   network       = google_compute_network.my_vpc.id
   ip_cidr_range = var.db_subnet_cidr
   region        = var.region
 }
 
 resource "google_compute_route" "vpc-route" {
-  name             = "vpc-route"
-  dest_range       = "0.0.0.0/0"
+  name             = var.router_name
+  dest_range       = var.router_dest_range
   network          = google_compute_network.my_vpc.name
-  next_hop_gateway = "default-internet-gateway"
+  next_hop_gateway = var.router_next_hop
 }
 
